169c6d5156bd106faeb8fcba559bf47ba30e91ec
Josh <joshuaal22@g.eips.ca>
2018-01-15 15:04:21 -0700

    Update wallpaper1.sh

--- Scripts/wallpaper1.sh
+++ Scripts/wallpaper1.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-read "What wallpaper do you want?\n\n1. Gray wallpaper\n2. Colorful wallpaper\n3. Other wallpaper\n\n(Select a number and press enter)"
+read "What wallpaper do you want?\n\n1. Gray PocketDESK wallpaper\n2. Plain Colorful wallpaper\n3. Colorful PocketDESK wallpaper\n\n(Select a number and press enter)" $wallpaper
+
+if [ $wallpaper == "1" ]
+  wget -O /home/chip/.pocket-home/wallpaper.png https://raw.githubusercontent.com/Jalonzpa/PocketDesk/master/images/PocketDESK_wall_gray_480x272.png
+elif [ $wallpaper == "2" ]
+  wget -O /home/chip/.pocket-home/wallpaper.png https://raw.githubusercontent.com/Jalonzpa/PocketDesk/master/images/8-bit.png
+elif [ $wallpaper == "3" ]
+  wget -O /home/chip/.pocket-home/wallpaper.png https://raw.githubusercontent.com/Jalonzpa/PocketDesk/master/images/PocketDESK_wall.png
