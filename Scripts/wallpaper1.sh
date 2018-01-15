ef2be4c69d987c4c0498c9b6849ae9e62df73711
Josh <joshuaal22@g.eips.ca>
2018-01-15 15:16:21 -0700

    Update wallpaper1.sh

--- Scripts/wallpaper1.sh
+++ Scripts/wallpaper1.sh
@@ -3,8 +3,9 @@
 read "What wallpaper do you want?\n\n1. Gray PocketDESK wallpaper\n2. Plain Colorful wallpaper\n3. Colorful PocketDESK wallpaper\n\n(Select a number and press enter)" $wallpaper
 
 if [ $wallpaper == "1" ]
-  wget -O /home/chip/.pocket-home/wallpaper.png https://raw.githubusercontent.com/Jalonzpa/PocketDesk/master/images/PocketDESK_wall_gray_480x272.png
+  then wget -O /home/chip/.pocket-home/wallpaper.png https://raw.githubusercontent.com/Jalonzpa/PocketDesk/master/images/PocketDESK_wall_gray_480x272.png
 elif [ $wallpaper == "2" ]
-  wget -O /home/chip/.pocket-home/wallpaper.png https://raw.githubusercontent.com/Jalonzpa/PocketDesk/master/images/8-bit.png
+  then wget -O /home/chip/.pocket-home/wallpaper.png https://raw.githubusercontent.com/Jalonzpa/PocketDesk/master/images/8-bit.png
 elif [ $wallpaper == "3" ]
-  wget -O /home/chip/.pocket-home/wallpaper.png https://raw.githubusercontent.com/Jalonzpa/PocketDesk/master/images/PocketDESK_wall.png
+  then wget -O /home/chip/.pocket-home/wallpaper.png https://raw.githubusercontent.com/Jalonzpa/PocketDesk/master/images/PocketDESK_wall.png
+fi
